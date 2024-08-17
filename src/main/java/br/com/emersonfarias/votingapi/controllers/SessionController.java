7fb065a017d54aa22b69c7d3d7cd281699967402
Emerson de Farias Santos <e.fariassantos@gmail.com>
2024-08-17 14:36:28 -0300

    created all voting controller is service.

--- src/main/java/br/com/emersonfarias/votingapi/controllers/SessionController.java
+++ src/main/java/br/com/emersonfarias/votingapi/controllers/SessionController.java
@@ -3,35 +3,41 @@ package br.com.emersonfarias.votingapi.controllers;
 
 import br.com.emersonfarias.votingapi.models.Session;
 import br.com.emersonfarias.votingapi.repositories.SessionRepository;
+import br.com.emersonfarias.votingapi.services.SessionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/session")
 public class SessionController {
-    private final SessionRepository sessionRepository;
-    public SessionController(SessionRepository sessionRepository) {
-        this.sessionRepository = sessionRepository;
+    private final SessionService sessionService;
+    public SessionController(SessionService sessionService) {
+        this.sessionService = sessionService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Session>> getAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(sessionService.findAll());
     }
 
     @PostMapping("/open")
-    public ResponseEntity<Session> abrirSessao() {
+    public ResponseEntity<Session> openSession() {
         Session session = new Session();
         session.setDateStart(LocalDateTime.now());
         session.setOpen(true);
-        return ResponseEntity.status(HttpStatus.CREATED).body(sessionRepository.save(session));
+        return ResponseEntity.status(HttpStatus.CREATED).body(sessionService.save(session));
     }
 
     @PatchMapping("/{id}/close")
-    public ResponseEntity<Session> fecharSessao(@PathVariable Long id) {
-        return sessionRepository.findById(id)
-                .map(sessao -> {
-                    sessao.setDateEnd(LocalDateTime.now());
-                    sessao.setOpen(false);
-                    return ResponseEntity.ok(sessionRepository.save(sessao));
+    public ResponseEntity<Session> closeSession(@PathVariable Long id) {
+        return sessionService.findById(id).map(session -> {
+                     session.setDateEnd(LocalDateTime.now());
+                    session.setOpen(false);
+                    return ResponseEntity.ok(sessionService.save(session));
                 })
                 .orElse(ResponseEntity.notFound().build());
     }
