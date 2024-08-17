8abff7268e97db61e03f94a11168e557c6ba9791
Emerson de Farias Santos <e.fariassantos@gmail.com>
2024-08-17 12:27:04 -0300

    commit initial of projects.

---
+++ src/main/java/br/com/emersonfarias/votingapi/VotingapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.emersonfarias.votingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/emersonfarias/votingapi/VotingapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emersonfarias.votingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
